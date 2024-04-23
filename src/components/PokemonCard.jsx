badae98716132d42e31801245439fe5695122b1b
Imanol Deleporte <mana.gyllenhaalic@gmail.com>
2024-04-23 15:51:06 +0200

    Add Data

--- src/components/PokemonCard.jsx
+++ src/components/PokemonCard.jsx
@@ -1,3 +1,23 @@
+const pokemonList = [
+
+  {
+
+    name: "bulbasaur",
+
+    imgSrc:
+
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+
+  },
+
+  {
+
+    name: "mew",
+
+  },
+
+];
+console.log (pokemonList);
 const PokemonCard = () => {
   return (
     <>
