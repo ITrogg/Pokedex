6af5597641a65412587dd3003c79e4f162748858
Imanol Deleporte <mana.gyllenhaalic@gmail.com>
2024-04-23 16:33:06 +0200

    Deplacer tableau

--- src/components/PokemonCard.jsx
+++ src/components/PokemonCard.jsx
@@ -1,26 +1,6 @@
-const pokemonList = [
-
-  {
-
-    name: "bulbasaur",
-
-    imgSrc:
-
-      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
-
-  },
-
-  {
-
-    name: "mew",
-
-  },
-
-];
-
 const PokemonCard = (props) => {
   console.log(props);
-  const pokemon = pokemonList[1];
+  const pokemon = { name: "mew" };
   return (
     <>
     <figure>
